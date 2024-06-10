10be708658000268afe6cd132c66bc1d4f7dbb0c
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-10 03:48:32 +0300

    Commit for JS Studying

---
+++ class013.js
@@ -0,0 +1,9 @@
+// Create an array of numbers from 1 to 20
+const numbers = [];
+for (let i = 1; i <= 20; i++) {
+    numbers.push(i);
+}
+// Alternatively, using forEach for iteration
+numbers.forEach(number => {
+    console.log(number);
+});
